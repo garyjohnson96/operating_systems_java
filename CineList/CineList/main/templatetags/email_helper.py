5a7d36080d2a65c389d5739531d1cab8e0adcef3
Gary Johnson <gary3450@gmail.com>
2019-05-01 14:09:15 -0600

    removed unwanted project

--- CineList/CineList/main/templatetags/email_helper.py
+++
@@ -1,8 +0,0 @@
-from django.template import Library
-
-register = Library()
-
-@register.filter(name='br_replace')
-def br_replace(string):
-    newstr = string.replace('\n', '<br/>')
-    return newstr
--- CineList/CineList/scripts/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python manage.py flush --no-input
-python manage.py makemigrations main
-python manage.py migrate
-python manage.py loaddata test_data.json
-
-python manage.py runserver
